95d1f70cbdd638ff94fc914dd1ef3a47723f5ed9
Edson <edsonjunior@ice.ufjf.br>
2019-02-12 20:08:41 -0200

    adicionar classes do monstro e de habilidade

---
+++ Assets/Scripts/MonsterPart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Monster Part", menuName = "Monster/Monster Part", order = 1)]
+public class MonsterPart : ScriptableObject
+{
+    int life;
+    int shield;
+    int movementSpeed;
+    int damage;
+    int partID;
+    public List <Ability> abilityList;
+    Image sprite;
+}
